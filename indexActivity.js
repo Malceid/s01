244361cc124294cf2c484b26b6cc46854a7be5d0
Mark DeJesus <malceidt@gmail.com>
2023-03-20 17:00:17 +0800

    230320

---
+++ indexActivity.js
@@ -0,0 +1,26 @@
+console.log("Hello, Welcome to De La Salle University!");
+
+
+function printAll(){
+let	firstName = "Mark";
+let	lastName = "De Jesus";
+let	age = 20;
+let	hobbies = ["Gaming", "Listening to Music", "Travelling", "Cycling", "Collecting Earphones", "Reading Manga"];
+let	homeAddress = {
+	 houseNumber: "Lot 28, Block B, Phase 4",
+	 street: "N/A",
+	 subdivision: "Filipinas Homes Paradise",
+	 barangay: "Sabang",
+	 city: "Naic",
+	 province: "Cavite",
+	 zipCode: 4110
+	}
+	console.log(firstName);
+	console.log(lastName);
+	console.log(age);
+	console.log(hobbies);
+	console.log(homeAddress);
+}
+
+printAll()
+
